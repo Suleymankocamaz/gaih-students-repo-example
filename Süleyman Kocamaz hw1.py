baa89506817cd16451adfd80cee3cd7049d5854b
Suleymankocamaz <80474170+Suleymankocamaz@users.noreply.github.com>
2021-03-11 19:55:48 +0300

    Add files via upload

---
+++ Süleyman Kocamaz hw1.py
@@ -0,0 +1,7 @@
+list1:list[int]=[1,8,4]
+list2:list[int]=[1,8,4]
+mergedlist = list1 + list2
+multmergedlist [i * 2 for i in mergedlist]
+
+for x in multmergedlist:
+    print(multmergedlist)
---
+++ Süleyman kocamaz hw 2.py
@@ -0,0 +1,12 @@
+dict1 ={"Name": "Suleyman", "Surname":"Kocamaz", "Age":21 ,"city":"Konya", "occupation":"Electrical engineer"}
+dict2 ={"Name": "Ertan", "Surname":"Alabay", "Age":21,"city":"Istanbul", "occupation":" Computer engineer"}
+dict3 ={"Name": "Enes", "Surname":"Sivas", "Age":21 ,"city":"Ankara", "occupation":"Industrial engineer"}
+dict4 ={"Name": "Yigit", "Surname":"Oztekin", "Age":21 ,"city":"Eskisehir", "occupation":"Bioengineer"}
+dict5 ={"Name": "Emin", "Surname":"Yuce", "Age":21 ,"city":"Izmir", "occupation":"Food engineer"}
+
+print(dict1)
+print(dict2)
+print(dict3)
+print(dict4)
+print(dict5)
+
